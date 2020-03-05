c8e35ca800298fa4d284c15a87e2a8b5b607a8a4
BananaLoaf <bananaloaf@protonmail.com>
2020-03-06 02:53:30 +0300

    Updated files

--- setup.py
+++ setup.py
@@ -6,15 +6,16 @@ except ModuleNotFoundError:
 from template import PAYLOAD
 
 
+with open("README.md", "r") as file:
+    LONG_DESCRIPTION = file.read()
+PAYLOAD["long_description"] = LONG_DESCRIPTION
+
+
 import json
 assert PAYLOAD["description"] == "This line is a description"
 assert json.dumps(PAYLOAD).find("TEMPLATE") == -1
 assert json.dumps(PAYLOAD).find("null") == -1
 
-
-with open("README.md", "r") as file:
-    LONG_DESCRIPTION = file.read()
-PAYLOAD["long_description"] = LONG_DESCRIPTION
 
 kwargs = {}
 for key in PAYLOAD:
