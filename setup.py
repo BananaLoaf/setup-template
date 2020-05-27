93347de61275558decae592ae601aaca63b4360e
BananaLoaf <bananaloaf@protonmail.com>
2020-05-27 19:04:43 +0300

    Updated files Updated files Updated files

--- setup.py
+++ setup.py
@@ -1,25 +1,53 @@
-try:
-    from setuptools import setup
-except ModuleNotFoundError:
-    from distutils.core import setup
+# Local
+from setuptools import setup, find_packages
 
-from template import PAYLOAD
+# External
+# Import numpy as np
+
+# Project
+from template import __version__
 
 
 with open("README.md", "r") as file:
     LONG_DESCRIPTION = file.read()
-PAYLOAD["long_description"] = LONG_DESCRIPTION
 
+# https://docs.python.org/2/distutils/setupscript.html
+setup(name="template-package",
+      version=__version__,
+      install_requires=["numpy"],
+      packages=find_packages(),
+      entry_points={
+          "console_scripts": [
+              "template = template.main:main",
+              "template-again = template.main:main"
+          ],
+          "gui_scripts": [  # As far is I know they get detached from terminal
+              "template = template.main:main"
+          ]
+      },
 
-import json
-assert PAYLOAD["description"] == "This line is a description"
-assert json.dumps(PAYLOAD).find("TEMPLATE") == -1
-assert json.dumps(PAYLOAD).find("null") == -1
+      # Metadata for PyPi
+      author="BananaLoaf",
+      author_email="bananaloaf@protonmail.com",
+      # maintainer="BananaLoaf",
+      # maintainer_email="bananaloaf@protonmail.com",
+      license="MIT",
 
+      description="This line is a description",
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type="text/markdown",
+      keywords=["template", "whatever"],
 
-kwargs = {}
-for key in PAYLOAD:
-    if PAYLOAD[key] is not None:
-        kwargs[key] = PAYLOAD[key]
+      url="https://github.com/BananaLoaf/setup-template",
+      # download_url=None,
+      project_urls={
+          "Lord and Saviour": "https://stackoverflow.com/"
+      },
 
-setup(**kwargs)
+      # https://pypi.org/classifiers/
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+      ])
